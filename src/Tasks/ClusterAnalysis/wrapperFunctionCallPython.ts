5d005f75d3a86f9e2624d38f59bcfbaf0deb7e12
Paul Christ <paul.l.christ@web.de>
2024-01-08 17:45:44 +0100

    fix dynamic layouting

--- src/Tasks/ClusterAnalysis/wrapperFunctionCallPython.ts
+++ src/Tasks/ClusterAnalysis/wrapperFunctionCallPython.ts
@@ -5,9 +5,8 @@ const path = require("path");
 function runPythonScript(scriptPath: string, args: any): Promise<string> {
 	try {
 		return new Promise((resolve, reject) => {
-            
 			const command = `python3 ${scriptPath} ${args.join(" ")}`;
-            console.log(command)
+			console.log(command);
 			exec(command, (error, stdout, stderr) => {
 				if (error) {
 					reject(error.message);
@@ -33,11 +32,11 @@ export async function clusterAnalysisMain(parameter: any) {
 		let pythonScriptPath = "clusterAnalysis.py";
 		pythonScriptPath = path.join(__dirname, pythonScriptPath);
 		//console.log(pythonScriptPath);
-        console.log(parameter['distance']);
+		console.log(parameter["distance"]);
 
-		const argumentsToPythonScript = [...Object.values(parameter),...parameter['nodeRange']];
-        //delete argumentsToPythonScript[2];
-        console.log(argumentsToPythonScript);
+		const argumentsToPythonScript = [...Object.values(parameter), ...parameter["nodeRange"]];
+		//delete argumentsToPythonScript[2];
+		console.log(argumentsToPythonScript);
 		(await runPythonScript(pythonScriptPath, argumentsToPythonScript)) as any;
 
 		//let foo = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf-8');
