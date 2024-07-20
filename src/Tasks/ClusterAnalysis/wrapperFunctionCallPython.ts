d8a40f33d595d37d5e5a3bc59421b0e6e3b45292
Mohamed Benhaddia <mohamed.benhaddia2@stud.htw-dresden.de>
2024-07-20 11:48:11 +0100

    chore: Add cluster analysis functionality and wrapper function for Python script

---
+++ src/Tasks/ClusterAnalysis/wrapperFunctionCallPython.ts
@@ -0,0 +1,51 @@
+import { exec } from "child_process";
+import fs from "fs";
+const path = require("path");
+
+function runPythonScript(scriptPath: string, args: any): Promise<string> {
+	try {
+		return new Promise((resolve, reject) => {
+			const command = `python3 ${scriptPath} ${args.join(" ")}`;
+			console.log(command);
+			exec(command, (error, stdout, stderr) => {
+				if (error) {
+					reject(error.message);
+					return;
+				}
+				if (stderr) {
+					reject(stderr);
+					return;
+				}
+				resolve(stdout);
+			});
+		});
+	} catch {
+		console.log("Error");
+	}
+}
+
+export async function clusterAnalysisMain(parameter: any) {
+	let result = { foo: "bar" };
+    console.log("PARAMETER:");
+	console.log(parameter);
+	try {
+		let pythonScriptPath = "clusterAnalysis.py";
+		pythonScriptPath = path.join(__dirname, pythonScriptPath);
+		//console.log(parameter["distance"]);
+
+		const argumentsToPythonScript = [...Object.values(parameter), ...parameter["nodeRange"]];
+		//delete argumentsToPythonScript[2];
+        //console.log("ARGUMENTS TO PYTHON SCRIPT");
+		//console.log(argumentsToPythonScript);
+		(await runPythonScript(pythonScriptPath, argumentsToPythonScript)) as any;
+
+		//let foo = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf-8');
+
+		result = JSON.parse(fs.readFileSync(path.join(__dirname, "data.json"), "utf-8"));
+        console.log(result);
+		//insofern kryptsiche Zeichen in der JSON Ausgabe vorhanden sind .> Workaround mittels File
+	} catch (error) {
+		console.error("Error running Python script:", error);
+	}
+	return result;
+}
