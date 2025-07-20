5a227d4a4eb1aa5f2a3d3ca5d3c12d6af9b059d6
Zakaria Lehmadi <s85625@htw-dresden.de>
2025-07-20 22:29:02 +0200

    Fix : Corrects the input indices used in two key calculations to ensure proper data mapping.

--- src/Tasks/Ferment/generateFermentationData.py
+++ src/Tasks/Ferment/generateFermentationData.py
@@ -46,7 +46,8 @@ eingabe = eingabeOb.get_Value()
 
 
 
-Sauerstoffoeslichkeit_array=Berechnung_der_Sauerstoffloeslichkeit(eingabe[1],eingabe[9],eingabe[3])
+
+Sauerstoffoeslichkeit_array=Berechnung_der_Sauerstoffloeslichkeit(eingabe[1],eingabe[10],eingabe[3])
    
     
 kla_werte=Berechnung_des_kla_Wertes(Sauerstoffoeslichkeit_array,modelle,eingabe[4],eingabe[0],eingabe[5],eingabe[1])
@@ -61,7 +62,7 @@ const_array=berechnung_der_Tabelle1(parameter_werte,eingabe[0],modelle,eingabe[5
 t_ranges_array= berechnung_der_Tabelle2(eingabe[2],eingabe[1])
     
     
-param_array=berechnung_der_Tabelle3(const_array,eingabe[10],eingabe[6],eingabe[8],eingabe[11],eingabe[1])
+param_array=berechnung_der_Tabelle3(const_array,eingabe[11],eingabe[6],eingabe[8],eingabe[9],eingabe[1])
     
     
 #Berechnung_ausführen
