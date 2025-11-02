287b1724b64162b85254fef6047f87da6223df38
Zakaria Lehmadi <s85625@htw-dresden.de>
2025-11-03 00:46:13 +0100

    CHG: Extended wrapper function generateExperimentierenDataMain: added resolveInputValue for LaTeX data and implemented saving via Utils function

--- src/Tasks/Experimentieren/wrapperFunctionCallPython.ts
+++ src/Tasks/Experimentieren/wrapperFunctionCallPython.ts
@@ -1,21 +1,65 @@
-import fs from "fs";
-const path = require("path");
 
-function saveFrontendUserData(userDataObject: Record<string, any>) : boolean{
-	const filePath = path.join(__dirname, "/internData/frontendUserData.json");
-    
-	let jsonString = JSON.stringify(userDataObject, null, 4);
-	
-	try {
-		fs.writeFileSync(filePath, jsonString, "utf-8");
-		return true;
-	} catch (error) {
-		return false;
-	}
+import { record } from "fp-ts";
+import { lastIndexOf } from "lodash";
+import { 
+	saveObjectAsJsonFile, 
+	getValueByPathFromJson,
+	callFunction
+} from "./Utils";
+
+import path from 'path';
+
+interface ILatexDataItem {
+	inputType		?: string;
+	value 			?: string;
+	selectedOption 	?: string; 
 }
 
+type LatexData = Record<string,ILatexDataItem>;
+
+
+function resolveInputValue(latexData: LatexData): LatexData | boolean {
+    if (Object.keys(latexData).length === 0) return false;
+
+    for (const key of Object.keys(latexData)) {
+        const elementData = latexData[key];
+
+        if (!elementData?.selectedOption) continue;
+
+        const option: string = elementData.selectedOption.toLowerCase();
+        if (!option || option.length === 0) continue;
+
+        switch (option) {
+            case 'vom pfad initialisieren': {
+                const [fileName, pathKey] = elementData.value.split(':');
+
+                const filePath = path.join(__dirname, 'internData', `${fileName}.json`);
+
+                const newValue: string = getValueByPathFromJson(filePath, pathKey);
+                elementData.value = newValue;
+                break;
+            }
+
+            case 'funktion aufruf': {
+                const newValue : string = callFunction(elementData.value);
+                elementData.value = newValue;
+                break;
+            }
+
+            default:
+                break;
+        }
+    }
+
+    return latexData;
+}
 
 export function generateExperimentierenDataMain(userDataObject: any) {
-    const check = saveFrontendUserData(userDataObject);
+    let latexData = resolveInputValue(userDataObject?.parameters?.latexData as LatexData);
+	
+	if(!latexData) return null;
+	userDataObject.parameters.latexData = latexData;
+	
+	const check = saveObjectAsJsonFile(userDataObject,`${__dirname}/internData/frontendUserData.json`);
     return check;
 }
